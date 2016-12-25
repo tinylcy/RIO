899540085b4f957f18c01a14c52fd74fd026fb65
tinylcy <tinylcy@gmail.com>
2016-12-25 22:58:53 +0800

    fix some wrong characters

--- rio.h
+++ rio.h
@@ -1,7 +1,7 @@
 #define RIO_BUFSIZE 8192
 
 typedef struct rio {
-	int rio_fd;                     /* secsriptor for this internal buf */
+	int rio_fd;                     /* sebsriptor for this internal buf */
 	int rio_cnt;                    /* unread bytes in internal buf */
 	char *rio_bufptr;               /* next unread byte in internal buf */
 	char rio_buf[RIO_BUFSIZE];      /* internal buffer */
